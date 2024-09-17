d31dc8bc9a993875f746cefe7cc8449e4c9b07dd
Vladyslav <122152653+h4ze-07@users.noreply.github.com>
2024-09-18 00:49:18 +0300

    finished adaptive

--- js/main.js
+++ js/main.js
@@ -4,10 +4,18 @@
 const productsList = document.querySelectorAll('.product-card .product-card__inner');
 productsList.forEach( (item, ind) => {
     if (ind % 2 != 0) {
-        
         item.classList.add('flex-rev')
     }
 })
 
 
-// .flex-rev 
+// burger + menu
+
+const burger = document.getElementById("burger");
+const mobileMenu = document.getElementById("mobileMenu");
+
+burger.addEventListener("click", () => {
+	burger.classList.toggle("active");
+	mobileMenu.classList.toggle("active");
+	document.body.classList.toggle("stopScroll");
+});
